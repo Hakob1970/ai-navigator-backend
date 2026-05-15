237db82f720c163f3b90cd383c981bb9b2976e71
Hakob1970 <haksmuradyan0@gmail.com>
2026-05-15 08:04:33 +0400

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -790,9 +790,8 @@ app.get("/api/admin/stats", async (req, res) => {
 
 
 // =========================
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 SERVER RUNNING ON", PORT);
 });
