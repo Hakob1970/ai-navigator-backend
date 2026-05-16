0a9ddda3bfa58daa09afe7a0ae5607be9aa359b8
Hakob1970 <haksmuradyan0@gmail.com>
2026-05-16 20:22:52 +0400

    Add middleware to log incoming requests

--- server.js
+++ server.js
@@ -254,6 +254,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  console.log("🔥 INCOMING:", req.method, req.url);
+  next();
+});
+
 app.post("/api/lemon/webhook", (req, res) => {
   console.log("🔥 LEMON WEBHOOK HIT");
   console.log("BODY:", req.body);
