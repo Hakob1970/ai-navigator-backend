58140fecef7c361a9e3269e047acfb47947a6863
Hakob1970 <haksmuradyan0@gmail.com>
2026-06-15 17:36:11 +0400

    Update autoMechanic.js

--- routes/autoMechanic.js
+++ routes/autoMechanic.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const pool = require("../server").pool;
+const pool = req.app.locals.pool;
 
 router.post("/", async (req, res) => {
     try {
