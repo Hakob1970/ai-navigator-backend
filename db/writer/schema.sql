9a044b30a9e259d13a11bfe4fd90f29d162478ba
Hakob1970 <haksmuradyan0@gmail.com>
2026-07-02 20:35:23 +0400

    Create writer_projects table in schema.sql

--- db/writer/schema.sql
+++ db/writer/schema.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS writer_projects (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    type TEXT NOT NULL,
+    title TEXT,
+    content TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
