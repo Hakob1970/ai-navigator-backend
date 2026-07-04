ff7a91567fb1c751864890290d08566d0846efb5
Hakob1970 <haksmuradyan0@gmail.com>
2026-07-04 09:31:22 +0400

    Add writer.js with content categories

---
+++ config/writer.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Books: ["Fantasy", "Sci-Fi", "Romance", "Historical", "Mystery"],
+  Education: ["Essay", "Research", "Homework", "Summary"],
+  Business: ["Email", "Proposal", "Marketing", "Business Plan"],
+  Content: ["Blog", "SEO", "YouTube", "Instagram"],
+  Creative: ["Characters", "Plot", "Dialogue", "World Builder"],
+  Tools: ["Rewrite", "Grammar", "Translate", "Humanize"]
+};
