6ea9853c289878277bf736b8a761af7cff1abf3f
Hakob1970 <haksmuradyan0@gmail.com>
2026-07-04 09:37:26 +0400

    Update writer.js

--- routes/writer.js
+++ routes/writer.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
 
+const writerController = require("../controllers/writerController");
+
+// =========================
+// GENERATE ROUTE
+// =========================
+router.post("/generate", writerController.generate);
+
+module.exports = router;
