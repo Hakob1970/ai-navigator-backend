77ed9f0dfd278e76a929d1d1722562346f072248
Hakob1970 <haksmuradyan0@gmail.com>
2026-07-06 20:56:18 +0400

    Create constants.js

---
+++ services/studio/engine/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ENGINE_MODES: {
+        BOOK: "book",
+        CHARACTER: "character",
+        WORLD: "world",
+        OUTLINE: "outline",
+        EDITOR: "editor"
+    }
+};
