58d31e9e974264877e70760121b6a36fe29c4cb9
Hakob1970 <haksmuradyan0@gmail.com>
2026-07-07 20:02:59 +0400

    Update promptRegistry.js

--- services/studio/promptRegistry.js
+++ services/studio/promptRegistry.js
@@ -1,5 +1,13 @@
 const fantasy = require("../../prompts/Books/fantasy");
+const historical = require("../../prompts/Books/historical");
+const mystery = require("../../prompts/Books/mystery");
+const romance = require("../../prompts/Books/romance");
+const scifi = require("../../prompts/Books/scifi");
 
 module.exports = {
-  Fantasy: fantasy
+    fantasy,
+    historical,
+    mystery,
+    romance,
+    scifi
 };
